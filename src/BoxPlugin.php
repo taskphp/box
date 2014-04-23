ba3e06751696f91f93522ad9f5a370977cc84409
Mike Fisher <mike@mbfisher.com>
2014-04-23 14:15:05 +0100

    Add BOX_PATH const to constructor

--- src/BoxPlugin.php
+++ src/BoxPlugin.php
@@ -7,8 +7,16 @@ use KevinGH\Box\Application;
 
 class BoxPlugin extends ApplicationPlugin implements PluginInterface
 {
-    public function __construct()
+    public function __construct($boxPath = './vendor/kherge/box')
     {
+        if (!defined('BOX_PATH')) {
+            if (file_exists($boxPath)) {
+                define('BOX_PATH', $boxPath);
+            } else {
+                throw new \InvalidArgumentException("Box library not found at $boxPath");
+            }
+        }
+
         parent::__construct(new Application);
     }
 }
